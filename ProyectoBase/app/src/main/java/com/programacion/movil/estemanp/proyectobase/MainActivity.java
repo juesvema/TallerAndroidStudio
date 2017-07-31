870ae0c1b6ba5f85cd9b84934237542c91cac7e5
Juan Velasquez <juesvema_12@hotmail.com>
2017-07-31 17:11:45 -0500

    primero

--- ProyectoBase/app/src/main/java/com/programacion/movil/estemanp/proyectobase/MainActivity.java
+++ ProyectoBase/app/src/main/java/com/programacion/movil/estemanp/proyectobase/MainActivity.java
@@ -2,12 +2,44 @@ package com.programacion.movil.estemanp.proyectobase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            text = (TextView)findViewById(R.id.texto);
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        public void onStart() {
+            super.onStart();
+            text.setText("Paso por el onStart");
+        }
+
+        public void onRestart() {
+            super.onRestart();
+            text.setText("Paso por el onRestart");
+        }
+
+        public void onResume() {
+            super.onResume();
+            text.setText("Paso por el onResume");
+        }
+
+        public void onPause() {
+            super.onPause();
+            text.setText("Paso por el onPause");
+        }
+
+        public void onStop() {
+            super.onStop();
+            text.setText("Paso por el onStop");
+        }
+
+        public void onDestroy() {
+            super.onStop();
+            text.setText("Paso por el onDestroy");
+        }
 }
